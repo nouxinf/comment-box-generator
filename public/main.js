fb0f8ae9bc45379a37667e39496eb50cc306dc1e
nouxinf <180769756+nouxinf@users.noreply.github.com>
2026-08-05 21:36:18 +0100

    add support for various languages (c, asm, sql etc)

--- public/main.js
+++ public/main.js
@@ -3,18 +3,26 @@ const textValue = () => document.getElementById("text").value;
 const banner = (
 	text,
 	width = document.getElementById("minwidth").value || 40, // fallback to 40
+	commentType = document.getElementById("commenttype").value || "python",
 ) => {
 	if (text.length + 2 > width) {
 		width = text.length + 6;
 	}
-	const outerLine = "# " + "=".repeat(width - 2);
+	const langMap = new Map([
+		["python", "#"],
+		["c", "//"],
+		["sql", "--"],
+		["asm", ";"],
+	]);
+	const commentChar = langMap.get(commentType);
+	const outerLine = `${commentChar} ` + "=".repeat(width - 2);
 	const innerSpace = width - 4;
 
 	const paddedText = ` ${text} `
 		.padStart(Math.floor((innerSpace + text.length + 2) / 2), " ")
 		.padEnd(innerSpace, " ");
 
-	const middleLine = `# =${paddedText}=`;
+	const middleLine = `${commentChar} =${paddedText}=`;
 	return `${outerLine}\n${middleLine}\n${outerLine}`;
 };
 
